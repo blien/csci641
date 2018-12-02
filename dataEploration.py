bbbb4f224199dac15f61c970a47cd45cd107c0ad
brandon <brandon@desktop>
2018-12-02 15:22:10 -0600

    Hello world importing numpy, scipy, and matplotlib

---
+++ dataEploration.py
@@ -0,0 +1,16 @@
+'''
+Created on Dec 2, 2018
+
+@author: brandon
+'''
+
+import sys
+import scipy as sp
+import numpy as np
+import matplotlib as plt
+
+def main():
+    print("Hello world!")
+    
+if __name__ == '__main__':
+    main()
