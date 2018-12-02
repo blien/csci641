9a5b025f42323d5047addbd72cd6a7940fbfbf8a
brandon <brandon@desktop>
2018-12-02 16:51:12 -0600

    Parsing for JSON files working

--- dataEploration.py
+++ dataEploration.py
@@ -8,9 +8,36 @@ import sys
 import scipy as sp
 import numpy as np
 import matplotlib as plt
+import json
+
+
+def readAndPrintFile():
+    with open('E:\\School\\CSCI-641\\tarfiles\\keys\\17\\1700000.txt', 'r') as f:
+        f.readline()
+        for line in f:
+            print(line)
+
+
+def readJsonFiles ():
+    print("reading E:\\School\\CSCI-641\\tarfiles\\keys\\17\\1700000.tx:")
+    json_data = []
+    with open("E:\\School\\CSCI-641\\tarfiles\\keys\\18\\1800011.txt") as json_file:
+        for line in json_file:
+            #print(line)
+            cur_jason = json.loads(line)
+            print (json.dumps(cur_jason, indent = 4, sort_keys=True))
+            json_data.append(cur_jason)
+
+            #json_data.append(json.load(line))
+    print("Finished reading file")
+
+    #print(json.dumps(json_data))
+    #print(json_data)
 
 def main():
     print("Hello world!")
-    
+    readJsonFiles()
+    # readAndPrintFile()
+
 if __name__ == '__main__':
     main()
