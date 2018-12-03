d15b8cb1f0efafb56b480559584bfb5b0bab8b26
brandon <brandon@desktop>
2018-12-02 22:18:58 -0600

    Traverse and find all JSON files

--- dataEploration.py
+++ dataEploration.py
@@ -9,7 +9,29 @@ import scipy as sp
 import numpy as np
 import matplotlib as plt
 import json
+import glob, os
+from pprint import pprint
+import fnmatch
 
+
+def get_keys(dl, keys_list):
+    if isinstance(dl, dict):
+        keys_list += dl.keys()
+        map(lambda x: get_keys(x, keys_list), dl.values())
+    elif isinstance(dl, list):
+        map(lambda x: get_keys(x, keys_list), dl)
+
+def findDataFiles():
+    for root, dirs, files in os.walk('E:\\School\\CSCI-641\\tarfiles\\keys'):
+        for file in files:
+            if file.endswith(".txt"):
+                 print(os.path.join(root, file))
+    
+    """
+    for file in os.listdir('E:\\School\\CSCI-641\\tarfiles\\keys'):
+        if file.endswith(".txt"):
+            print(os.path.join('E:\\School\\CSCI-641\\tarfiles\\keys', file))
+    """
 
 def readAndPrintFile():
     with open('E:\\School\\CSCI-641\\tarfiles\\keys\\17\\1700000.txt', 'r') as f:
@@ -25,7 +47,15 @@ def readJsonFiles ():
         for line in json_file:
             #print(line)
             cur_jason = json.loads(line)
-            print (json.dumps(cur_jason, indent = 4, sort_keys=True))
+            #print (json.dumps(cur_jason, indent = 4, sort_keys=True))
+            """
+            for key, value in cur_jason.items():
+                pprint(key)
+            """
+            keys = []
+            get_keys(cur_jason, keys)
+            
+            print(keys)
             json_data.append(cur_jason)
 
             #json_data.append(json.load(line))
@@ -36,8 +66,10 @@ def readJsonFiles ():
 
 def main():
     print("Hello world!")
-    readJsonFiles()
+    findDataFiles()
+    # readJsonFiles()
     # readAndPrintFile()
+    print("End of file")
 
 if __name__ == '__main__':
     main()
